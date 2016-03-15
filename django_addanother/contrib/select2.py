5c509b32af7a65bd121cb55d3d844b0b721db7a6
Jonas Haag <jonas@lophus.org>
2016-03-15 20:44:13 +0100

    pep8

--- django_addanother/contrib/select2.py
+++ django_addanother/contrib/select2.py
@@ -11,6 +11,7 @@ from django_select2.forms import (
 
 from ..widgets import AddAnotherWidgetWrapper
 
+
 class Select2WidgetWrapper(AddAnotherWidgetWrapper):
     def __init__(self, add_related_url, add_icon=None):
         super(Select2WidgetWrapper, self).__init__(
@@ -19,26 +20,34 @@ class Select2WidgetWrapper(AddAnotherWidgetWrapper):
             add_icon,
         )
 
+
 class Select2AddAnother(Select2WidgetWrapper):
     widget_class = Select2Widget
+
 
 class HeavySelect2AddAnother(Select2WidgetWrapper):
     widget_class = HeavySelect2Widget
 
+
 class Select2MultipleAddAnother(Select2WidgetWrapper):
     widget_class = Select2MultipleWidget
+
 
 class HeavySelect2MultipleAddAnother(Select2AddAnother):
     widget_class = HeavySelect2MultipleWidget
 
+
 class HeavySelect2TagAddAnother(Select2AddAnother):
     widget_class = HeavySelect2TagWidget
+
 
 class ModelSelect2AddAnother(Select2AddAnother):
     widget_class = ModelSelect2Widget
 
+
 class ModelSelect2MultipleAddAnother(Select2AddAnother):
     widget_class = ModelSelect2MultipleWidget
+
 
 class ModelSelect2TagAddAnother(Select2AddAnother):
     widget_class = ModelSelect2TagWidget
