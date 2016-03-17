90e9bee32f26ac810710295ac4f04227c9e36df4
Jonas Haag <jonas@lophus.org>
2016-03-17 19:14:22 +0100

    Fix list of files that are distributed

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -8,7 +8,7 @@ setup(
     version='1.0.0',
     author='Jonas Haag, James Pic',
     author_email='jonas@lophus.org, jamespic@gmail.com',
-    packages=['django_addanother'],
+    packages=find_packages(exclude=['test_project']),
     zip_safe=False,
     include_package_data=True,
     url='https://github.com/jonashaag/django-addanother',
