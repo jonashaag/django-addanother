7d56cba8c4fc4e8bf3fab82b6c3384262bb7d2eb
Olivier <olivier.dalang@gmail.com>
2016-06-15 03:54:19 +0000

    refactoring

--- django_addanother/widgets.py
+++ django_addanother/widgets.py
@@ -72,20 +72,24 @@ class RelatedWidgetWrapper(WidgetWrapperMixin, forms.Widget):
     def render(self, name, value, *args, **kwargs):
         self.widget.choices = self.choices
 
-        add_params_separator = '&' if (self.add_related_url and '?' in self.add_related_url) else '?'
-        edit_params_separator = '&' if (self.edit_related_url and '?' in self.edit_related_url) else '?'
-
         url_params = "%s=%s" % (IS_POPUP_VAR, 1)
+
+        add_related_url = None
+        if self.add_related_url:
+            separator = '&' if '?' in self.add_related_url else '?'
+            add_related_url = self.add_related_url + separator + url_params
+
+        edit_related_url = None
+        if self.edit_related_url:
+            separator = '&' if '?' in self.edit_related_url else '?'
+            edit_related_url = self.edit_related_url + separator + url_params
 
         context = {
             'widget': self.widget.render(name, value, *args, **kwargs),
             'name': name,
-            'url_params': url_params,
-            'add_related_url': self.add_related_url,
-            'add_params_separator': mark_safe(add_params_separator),
+            'add_related_url': mark_safe(add_related_url),
             'add_icon': self.add_icon,
-            'edit_related_url': self.edit_related_url,
-            'edit_params_separator': mark_safe(edit_params_separator),
+            'edit_related_url': mark_safe(edit_related_url),
             'edit_icon': self.edit_icon,
         }
         return mark_safe(render_to_string(self.template, context))
