1bfa49f0984025ca6def90a6b235268b6a075b47
Jonas Haag <jonas@lophus.org>
2016-06-20 23:55:17 +0200

    Grammar

--- test_project/testapp/models.py
+++ test_project/testapp/models.py
@@ -14,15 +14,15 @@ class Player(models.Model):
     name = models.CharField(max_length=20)
     current_team = models.ForeignKey(
         "Team", related_name="current_players",
-        help_text="This demonstrate the wrapper adding a create button only"
+        help_text='This demonstrates the wrapper adding an "add" button only'
     )
     future_team = models.ForeignKey(
         "Team", related_name="future_players",
-        help_text="This demonstrate the wrapper adding both a create and an edit button"
+        help_text='This demonstrates the wrapper adding both an "add" and an "edit" button'
     )
     previous_teams = models.ManyToManyField(
         "Team", related_name="ancient_players",
-        help_text="This demonstrate the wrapper on a ManyToMany field"
+        help_text="This demonstrates the wrapper on a ManyToMany field"
     )
 
     def __str__(self):
