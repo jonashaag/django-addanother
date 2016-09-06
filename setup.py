7af32bb2f0a38981f5ecbecb7e134684bdf04913
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2016-09-06 10:52:37 -0300

    remover install_requires=['django'] on setup.py
    fix issue:
    https://github.com/jonashaag/django-addanother/issues/24

--- setup.py
+++ setup.py
@@ -20,6 +20,5 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-    ],
-    install_requires=['django'],
+    ]
 )
