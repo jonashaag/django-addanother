2a4bade0e59528d06441995e0493dec76bbdbe5c
Jonas Haag <jonas@lophus.org>
2022-02-05 22:46:03 +0100

    Create run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+
+DEPS="pytest pytest-django pytest-cov pytest-splinter"
+if python --version | grep -q "Python 2.7"; then
+  DEPS="$DEPS django-appconf<=1.0"
+fi
+if $DJANGO_VERSION = 1.11; then
+  DEPS="$DEPS django-select2<7"
+else
+  DEPS="$DEPS django-select2"
+fi
+if $DJANGO_VERSION = 1.11; then DEPS="$DEPS django>=1.11,<2"
+elif $DJANGO_VERSION = 2.0; then DEPS="$DEPS django>=2.0,<2.1"
+elif $DJANGO_VERSION = 2.1; then DEPS="$DEPS django>=2.1,<3"
+elif $DJANGO_VERSION = 3.0; then DEPS="$DEPS django>=3.0,<3.1"
+elif $DJANGO_VERSION = 3.1; then DEPS="$DEPS django>=3.1,<4"
+elif $DJANGO_VERSION = 4.0; then DEPS="$DEPS django>=4,<5"
+fi
+
+pip install $DEPS
+
+DJANGO_SETTINGS_MODULE=test_project.settings py.test -vv --cov django_addanother --create-db --strict -r fEsxXw test_project/testapp/tests.py
