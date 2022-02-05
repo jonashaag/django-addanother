966e489f7454d0717de45e8bb7e9c2b6184043ed
Jonas Haag <jonas@lophus.org>
2022-02-05 23:41:40 +0100

    GitHub Actions CI

---
+++ .github/workflows/make-requirements.txt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eu
+
+DEPS="wheel pytest pytest-django pytest-cov pytest-splinter"
+
+if python --version | grep -q "Python 2.7"; then
+  DEPS="$DEPS django-appconf<=1.0"
+fi
+
+if   [ $DJANGO_VERSION = 1.11 ]; then DEPS="$DEPS django>=1.11,<2   django-select2<7"
+elif [ $DJANGO_VERSION = 2.0  ]; then DEPS="$DEPS django>=2.0,<2.1  django-select2<7.2"
+elif [ $DJANGO_VERSION = 2.1  ]; then DEPS="$DEPS django>=2.1,<3    django-select2<7.2"
+elif [ $DJANGO_VERSION = 3.0  ]; then DEPS="$DEPS django>=3.0,<3.1  django-select2<7.5"
+elif [ $DJANGO_VERSION = 3.1  ]; then DEPS="$DEPS django>=3.1,<4    django-select2"
+elif [ $DJANGO_VERSION = 4.0  ]; then DEPS="$DEPS django>=4,<5      django-select2"
+else echo "Unknown Django version $DJANGO_VERSION"; exit 1
+fi
+
+echo $DEPS | tr ' ' '\n'
--- test_project/testapp/tests.py
+++ test_project/testapp/tests.py
@@ -1,7 +1,8 @@
 import functools
 import pytest
 from django_addanother.contrib import select2 as da_select2
-from testapp.models import Team
+from django import forms
+from testapp.models import Team, Player
 from testapp.forms import PlayerForm
 
 
@@ -14,24 +15,36 @@ def test_widget_deepcopy():
 
 
 @pytest.mark.django_db
-def test_smoke_select2():
+@pytest.mark.parametrize("widget_cls_name", da_select2.__all__)
+def test_smoke_select2(widget_cls_name):
     """Some basic tests to verify the select2 integration works."""
-    for widget_cls_name in da_select2.__all__:
-        if 'Heavy' in widget_cls_name:
-            # Need extra select2 specific arguments
-            continue
+    if 'Heavy' in widget_cls_name:
+        # Need extra select2 specific arguments
+        return
+    widget_cls = getattr(da_select2, widget_cls_name)
+    if 'AddAnotherEditSelected' in widget_cls_name:
+        widget = widget_cls(add_related_url='x', edit_related_url='x', add_icon='x', edit_icon='x')
+    elif 'AddAnother' in widget_cls_name:
+        widget = widget_cls(add_related_url='x', add_icon='x')
+    else:
+        widget = widget_cls(edit_related_url='x', edit_icon='x')
+    widget.widget.data_url = 'fake-url'
+    widget.widget.queryset = Player.objects.all()
 
-        widget_cls = getattr(da_select2, widget_cls_name)
-        if 'AddAnotherEditSelected' in widget_cls_name:
-            widget_cls(add_related_url='x', edit_related_url='x', add_icon='x', edit_icon='x')
-        elif 'AddAnother' in widget_cls_name:
-            widget_cls(add_related_url='x', add_icon='x')
-        else:
-            widget_cls(edit_related_url='x', edit_icon='x')
+    class TestForm(forms.ModelForm):
+        class Meta:
+            model = Player
+            fields = ['current_team']
+            widgets = {'current_team': widget}
+
+    html = TestForm().as_p()
+
+    # https://github.com/jonashaag/django-addanother/pull/47
+    assert not '<option value="" selected>' in html
 
 
 @pytest.mark.django_db
-def test_empty_select_multiple(session_browser, live_server):
+def test_browser_empty_select_multiple(session_browser, live_server):
     add_team = functools.partial(_add_team, session_browser, 'previous_teams')
     get_value = functools.partial(_get_value, session_browser, 'previous_teams')
     get_value_label = functools.partial(_get_value_label, session_browser, 'previous_teams')
@@ -53,7 +66,7 @@ def test_empty_select_multiple(session_browser, live_server):
 
 
 @pytest.mark.django_db
-def test_empty_foreign_key(session_browser, live_server):
+def test_browser_empty_foreign_key(session_browser, live_server):
     add_team = functools.partial(_add_team, session_browser, 'future_team')
     get_value = functools.partial(_get_value, session_browser, 'future_team')
     get_value_label = functools.partial(_get_value_label, session_browser, 'future_team')
