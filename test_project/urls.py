52f19051c75f1891de0c24c2ea1008f18d88786b
Jonas Haag <jonas@lophus.org>
2022-02-06 18:43:46 +0100

    WIP

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import url
+try:
+    from django.conf.urls import url as re_path
+except ImportError:
+    from django.conf.urls import re_path
 from django.contrib import admin
 from testapp.views import CreateTeam, EditTeam, main
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^team/add/$', CreateTeam.as_view(), name='add_team'),
-    url(r'^team/edit/(?P<pk>.*)/$', EditTeam.as_view(), name='edit_team'),
-    url(r'^$', main, name='main'),
+    path(r'^admin/', admin.site.urls),
+    path(r'^team/add/$', CreateTeam.as_view(), name='add_team'),
+    path(r'^team/edit/(?P<pk>.*)/$', EditTeam.as_view(), name='edit_team'),
+    path(r'^$', main, name='main'),
 ]
