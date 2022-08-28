e5c6c8c7461493e0e4594aff1fa2658e6ffce963
N1K1TAS95 <46042802+N1K1TAS95@users.noreply.github.com>
2022-08-28 18:20:52 +0200

    Django 4.1 support (#55)
    Co-authored-by: Jonas Haag <jonas@lophus.org>

--- .github/workflows/make-requirements.txt.sh
+++ .github/workflows/make-requirements.txt.sh
@@ -5,7 +5,7 @@ DEPS="wheel pytest pytest-django pytest-cov"
 if [ $PYTHON_VERSION = 2.7 ]; then
   DEPS="$DEPS django-appconf<=1.0 pytest-splinter<3 splinter<0.17"
 else
-  DEPS="$DEPS django-appconf pytest-splinter"
+  DEPS="$DEPS django-appconf git+https://github.com/pytest-dev/pytest-splinter"  # TODO: Switch back to PyPI https://github.com/pytest-dev/pytest-splinter/commit/0f9cae60055399bff9e3f3ce2c039d9c38f28e21
 fi
 
 if   [ $DJANGO_VERSION = 1.11 ]; then DEPS="$DEPS django>=1.11,<2   django-select2<7"
@@ -13,7 +13,8 @@ elif [ $DJANGO_VERSION = 2.0  ]; then DEPS="$DEPS django>=2.0,<2.1  django-selec
 elif [ $DJANGO_VERSION = 2.1  ]; then DEPS="$DEPS django>=2.1,<3    django-select2<7.2"
 elif [ $DJANGO_VERSION = 3.0  ]; then DEPS="$DEPS django>=3.0,<3.1  django-select2<7.5"
 elif [ $DJANGO_VERSION = 3.1  ]; then DEPS="$DEPS django>=3.1,<4    django-select2"
-elif [ $DJANGO_VERSION = 4.0  ]; then DEPS="$DEPS django>=4,<5      django-select2"
+elif [ $DJANGO_VERSION = 4.0  ]; then DEPS="$DEPS django>=4.0,<4.1  django-select2"
+elif [ $DJANGO_VERSION = 4.1  ]; then DEPS="$DEPS django>=4.1,<5    django-select2"
 else echo "Unknown Django version $DJANGO_VERSION"; exit 1
 fi
 
